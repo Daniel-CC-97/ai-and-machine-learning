c199b4810de7311fc2bb6bb3863fde6bec28a9f1
Daniel Cassell <danielcassell@Daniels-MacBook-Pro.local>
2024-08-13 13:34:14 +0100

    Style home page

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,11 +1,11 @@
 <!-- src/components/HeroSection.vue -->
 <template>
-  <section class="hero bg-blue-600 text-white py-12 px-6">
+  <section class="hero text-white min-h-screen py-12 px-6">
     <div class="container mx-auto text-center">
-      <h1 class="text-4xl font-bold mb-4">
+      <h1 class="text-4xl text-white font-bold mb-4">
         Discover the Future of AI with Our Interactive Showcase
       </h1>
-      <p class="text-lg mb-8">
+      <p class="text-lg text-gray-400 mb-8">
         Welcome to <strong>{{ appName }}</strong
         >, an innovative platform designed to bring the power of artificial
         intelligence and machine learning to life. Explore a diverse range of
@@ -15,7 +15,7 @@
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         <router-link
           to="/image-classification"
-          class="feature bg-white text-black p-6 rounded-lg shadow-lg"
+          class="feature bg-gray-700 text-white p-6 rounded-lg shadow-lg"
         >
           <h2 class="text-xl font-semibold mb-2">Image Classification</h2>
           <p class="text-base">
@@ -25,7 +25,7 @@
         </router-link>
         <router-link
           to="/text-generation"
-          class="feature bg-white text-black p-6 rounded-lg shadow-lg"
+          class="feature bg-gray-700 text-white p-6 rounded-lg shadow-lg"
         >
           <h2 class="text-xl font-semibold mb-2">Text Generation</h2>
           <p class="text-base">
@@ -35,7 +35,7 @@
         </router-link>
         <router-link
           to="/sentiment-analysis"
-          class="feature bg-white text-black p-6 rounded-lg shadow-lg"
+          class="feature bg-gray-700 text-white p-6 rounded-lg shadow-lg"
         >
           <h2 class="text-xl font-semibold mb-2">Sentiment Analysis</h2>
           <p class="text-base">
@@ -45,7 +45,7 @@
         </router-link>
         <router-link
           to="/object-detection"
-          class="feature bg-white text-black p-6 rounded-lg shadow-lg"
+          class="feature bg-gray-700 text-white p-6 rounded-lg shadow-lg"
         >
           <h2 class="text-xl font-semibold mb-2">Object Detection</h2>
           <p class="text-base">
@@ -55,7 +55,7 @@
         </router-link>
         <router-link
           to="/recommendation-system"
-          class="feature bg-white text-black p-6 rounded-lg shadow-lg"
+          class="feature bg-gray-700 text-white p-6 rounded-lg shadow-lg"
         >
           <h2 class="text-xl font-semibold mb-2">Recommendation System</h2>
           <p class="text-base">
@@ -64,7 +64,7 @@
           </p>
         </router-link>
       </div>
-      <p class="mt-8 text-lg">
+      <p class="mt-8 text-lg text-gray-400">
         Whether you're a tech enthusiast, a developer, or simply curious about
         the latest advancements in AI, our app offers a hands-on experience to
         explore and learn about these groundbreaking technologies.
@@ -86,7 +86,7 @@ export default {
 
 <style scoped>
 .hero {
-  background-color: #1e40af; /* Tailwind color for bg-blue-600 */
+  background-color: #000000;
 }
 
 .feature {
@@ -95,6 +95,5 @@ export default {
 
 .feature:hover {
   transform: translateY(-5px);
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
 }
 </style>
